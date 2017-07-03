2e8ff55a7e72a2f7cbe1937894efd17d95d4a005
Maria De Angelis <mariadeangelis@fb.com>
2017-07-03 16:39:28 -0700

    Added mentions tab

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsPagerAdapter.java
@@ -0,0 +1,10 @@
+package com.codepath.apps.restclienttemplate.fragments;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by mariadeangelis on 7/3/17.
+ */
+
+public class TweetsPagerAdapter extends FragmentPagerAdapter {
+}
